73f6d4e88d0d4476f2c13bad33b190e682f57dac
sunny9570 <sundy957063@gmail.com>
2024-06-25 17:27:50 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -12,18 +12,34 @@ $(function () {
     const mainVisualSlide = new Swiper('.main_visual_slide', {
         loof: true,
 
-    })
+    });
 
-    // $('#bgndVideo').YTPlayer({
-    //     videoURL: 'https://youtu.be/Sxf3GovRy-4',
-    //     containment: '.main_movie',
-    //     // 아래 뒤에 값 없애고 showControls: false더하면 유투브재생버튼없어짐
-    //     showControls: false,
-    //     playOnlyIfVisible: true,
-    //     // playOnlyIfVisible동영상이 돌아갈때만 재생
+    $('#bgndVideo01').YTPlayer({
+        videoURL: 'https://youtu.be/J-bJDSwTQMM',
+        containment: '.main_movie01',
+        showControls: false,
+        playOnlyIfVisible: true,
 
-    // });
+    });
+
+    $('#bgndVideo02').YTPlayer({
+        videoURL: 'https://youtu.be/_-mmbLYbmic',
+        containment: '.main_movie02',
+        // 아래 뒤에 값 없애고 showControls: false더하면 유투브재생버튼없어짐
+        showControls: false,
+        playOnlyIfVisible: true,
+        // playOnlyIfVisible동영상이 돌아갈때만 재생
+
+    });
 
     // let sw = true;
+
+    $('#bgndVideo03').YTPlayer({
+        videoURL: 'https://youtu.be/xn7md_oi0pA',
+        containment: '.main_movie03',
+        showControls: false,
+        playOnlyIfVisible: true,
+
+    });
 
 })
