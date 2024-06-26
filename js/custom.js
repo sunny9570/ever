245aea6b0b72be1289a18dc0302ea267adfaa4eb
sunny9570 <sundy957063@gmail.com>
2024-06-26 17:22:09 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -56,4 +56,26 @@ $(function () {
         },
     });
 
+
+
+    $('.left_slide').slick({
+
+        arrows: false,
+        fade: true,
+        asNavFor: '.right_slide'
+    });
+
+    $('.right_slide').slick({
+        arrows: false,
+        slidesToShow: 4,
+        asNavFor: '.left_slide',
+    });
+
+    $('.main_two_slide .arrows .prev').on('click', function () {
+        $('.left_slide').slick('slickPrev')
+    });
+    $('.main_two_slide .arrows .next').on('click', function () {
+        $('.left_slide').slick('slickNext')
+    });
+
 })
